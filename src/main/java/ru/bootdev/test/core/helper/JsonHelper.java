90f48d5a68e9db9c28aa05dafffe58612de0cf75
Said Umarsaidov <x7z3@mail.ru>
2021-05-07 22:59:27 +0300

    New helper methods

--- src/main/java/ru/bootdev/test/core/helper/JsonHelper.java
+++ src/main/java/ru/bootdev/test/core/helper/JsonHelper.java
@@ -6,24 +6,57 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class JsonHelper {
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-    public Boolean compareJsons(String json1, String json2) throws JsonProcessingException {
-        return compareJsonNodes(mapper.readTree(json1), mapper.readTree(json2));
+    public static Boolean compareJsons(String json1, String json2) throws JsonProcessingException {
+        return compareJsonNodes(getJsonNode(json1), getJsonNode(json2));
     }
 
-    public Boolean compareJsons(File json1, String json2) throws IOException {
-        return compareJsonNodes(mapper.readTree(json1), mapper.readTree(json2));
+    public static Boolean compareJsons(File json1, String json2) throws IOException {
+        return compareJsonNodes(getJsonNode(json1), getJsonNode(json2));
     }
 
-    public Boolean compareJsons(File json1, File json2) throws IOException {
-        return compareJsonNodes(mapper.readTree(json1), mapper.readTree(json2));
+    public static Boolean compareJsons(File json1, File json2) throws IOException {
+        return compareJsonNodes(getJsonNode(json1), getJsonNode(json2));
     }
 
-    private Boolean compareJsonNodes(JsonNode jsonNode1, JsonNode jsonNode2) {
+    private static Boolean compareJsonNodes(JsonNode jsonNode1, JsonNode jsonNode2) {
         return jsonNode1.equals(jsonNode2);
+    }
+
+    public static String objectToJsonString(Object object) throws JsonProcessingException {
+        return mapper.writeValueAsString(object);
+    }
+
+    public static <T> T jsonStringToObject(String jsonString, Class<T> clazz) throws JsonProcessingException {
+        return mapper.readValue(jsonString, clazz);
+    }
+
+    public static <T> T jsonFileToObject(File jsonFile, Class<T> clazz) throws IOException {
+        return mapper.readValue(jsonFile, clazz);
+    }
+
+    public static <T> T jsonNodeToObject(JsonNode jsonNode, Class<T> clazz) throws JsonProcessingException {
+        return mapper.treeToValue(jsonNode, clazz);
+    }
+
+    public static void objectToJsonFile(Object object, File jsonFile) throws IOException {
+        mapper.writeValue(jsonFile, object);
+    }
+
+    public static JsonNode getJsonNode(String jsonString) throws JsonProcessingException {
+        return mapper.readTree(jsonString);
+    }
+
+    public static JsonNode getJsonNode(File jsonString) throws IOException {
+        return mapper.readTree(jsonString);
+    }
+
+    public static JsonNode getJsonNode(URL url) throws IOException {
+        return mapper.readTree(url);
     }
 }
---
+++ src/main/java/ru/bootdev/test/core/helper/TextHelper.java
@@ -0,0 +1,8 @@
+package ru.bootdev.test.core.helper;
+
+public class TextHelper {
+
+    public static String generateId() {
+        return String.valueOf(System.nanoTime());
+    }
+}
