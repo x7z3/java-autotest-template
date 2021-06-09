0f030ca137638d332fd1e8b255fa2c6f32fcfdef
Said Umarsaidov <x7z3@mail.ru>
2021-06-09 13:05:30 +0300

    Add new methods

--- src/main/java/ru/bootdev/test/core/helper/TextHelper.java
+++ src/main/java/ru/bootdev/test/core/helper/TextHelper.java
@@ -1,8 +1,22 @@
 package ru.bootdev.test.core.helper;
 
+import java.util.Random;
+
 public class TextHelper {
 
     public static String generateId() {
         return String.valueOf(System.nanoTime());
+    }
+
+    public static String generateRandomId(int length, int randomNumberOrigin, int randomNumberBound) {
+        if (length <= 0) return "";
+        StringBuilder buffer = new StringBuilder();
+        new Random(System.nanoTime()).ints(length, randomNumberOrigin, randomNumberBound)
+                .forEach(i -> buffer.append(buffer.length() == 0 ? 1 : i));
+        return buffer.toString();
+    }
+
+    public static String generateRandomId(int length) {
+        return generateRandomId(length, 0, 9);
     }
 }
